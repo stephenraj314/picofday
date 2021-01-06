dccce45731f1db09dd07167b93a2298d3884e84c
stephen <stephen@gma>
2021-01-06 16:23:27 +0530

    Adding comments

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from bs4 import BeautifulSoup
 from wallpaper import set_wallpaper
 
 response=requests.get('https://commons.wikimedia.org/wiki/Main_Page')
-print(response.status_code)
+# Checking website status
 if response.status_code == 200:
     print('Success!')
 elif response.status_code == 404:
-    print('Not Found.')
+    print('Page Not Found.')
 src=response.content
 soup = BeautifulSoup(src,'lxml')
+# finding src of pic of the day image
 ss=soup.find('div',{'id':'mf-picture-picture','class':'mainpage-box-content'})
-print(ss.a['href'])
 url="https://commons.wikimedia.org{}".format(ss.a['href'])
 imagepage=requests.get(url)
 imgsoup = BeautifulSoup(imagepage.content,'lxml')
+# getting image url for original file source
 imgurl=imgsoup.find('a',string="Original file")['href']
+# downloading image file
 with open('wall.jpg','wb') as handle:
     response = requests.get(imgurl).content
     handle.write(response)
+# setting image file as desktop wallpaper
 set_wallpaper("wall.jpg")
